e3f633a3e2b1cbc75cbb2efe0326c869ef139172
Max Lapan <max.lapan@gmail.com>
2019-01-24 11:41:58 +0300

    Training prototype

--- libtraffic/model.py
+++ libtraffic/model.py
@@ -29,3 +29,36 @@ class DQN(nn.Module):
     def forward(self, x):
         conv_out = self.conv(x).view(x.size()[0], -1)
         return self.fc(conv_out)
+
+
+def unpack_batch(batch):
+    states, actions, rewards, dones, last_states = [], [], [], [], []
+    for exp in batch:
+        state = np.array(exp.state, copy=False)
+        states.append(state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        dones.append(exp.last_state is None)
+        if exp.last_state is None:
+            last_states.append(state)       # the result will be masked anyway
+        else:
+            last_states.append(np.array(exp.last_state, copy=False))
+    return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), \
+           np.array(dones, dtype=np.uint8), np.array(last_states, copy=False)
+
+
+def calc_loss_dqn(batch, net, tgt_net, gamma, device="cpu"):
+    states, actions, rewards, dones, next_states = unpack_batch(batch)
+
+    states_v = torch.tensor(states).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    done_mask = torch.ByteTensor(dones).to(device)
+
+    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+    next_state_values = tgt_net(next_states_v).max(1)[0]
+    next_state_values[done_mask] = 0.0
+
+    expected_state_action_values = next_state_values.detach() * gamma + rewards_v
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
--- train_model.py
+++ train_model.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import logging
-import pathlib
 
 import ptan
+import numpy as np
 import gym.wrappers
 from tensorboardX import SummaryWriter
 from libtraffic import env, utils, model
+
+import torch.optim as optim
 
 log = logging.getLogger("train_model")
 
@@ -15,7 +17,18 @@ LANES_SIDE = 1
 PATCHES_AHEAD = 20
 PATCHES_BEHIND = 10
 HISTORY = 3
+
 ENV_STEPS_LIMIT = 1000
+EPS_START = 1.0
+EPS_END = 0.05
+EPS_STEPS = 10000
+
+GAMMA = 0.95
+REPLAY_SIZE = 1000
+MIN_REPLAY = 256
+LEARNING_RATE = 1e-4
+BATCH_SIZE = 128
+NET_SYNC_STEPS = 128
 
 
 if __name__ == "__main__":
@@ -23,6 +36,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", required=True, help="Name of the run")
     args = parser.parse_args()
+
+    writer = SummaryWriter(comment="-" + args.name)
 
     e = env.DeepTraffic(lanes_side=LANES_SIDE, patches_ahead=PATCHES_AHEAD,
                         patches_behind=PATCHES_BEHIND, history=HISTORY)
@@ -33,4 +48,36 @@ if __name__ == "__main__":
     net = model.DQN(obs_shape, e.action_space.n)
     log.info("Model: %s", net)
 
+    tgt_net = ptan.agent.TargetNet(net)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=EPS_START)
+    agent = ptan.agent.DQNAgent(net, selector)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(e, agent, gamma=GAMMA, steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    step = 0
+    losses = []
+    with ptan.common.utils.RewardTracker(writer) as tracker:
+        while True:
+            buffer.populate(1)
+            if len(buffer) < MIN_REPLAY:
+                continue
+            step += 1
+            selector.epsilon = max(EPS_END, EPS_START - step / EPS_STEPS)
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                tracker.reward(new_rewards[-1], step, epsilon=selector.epsilon)
+            optimizer.zero_grad()
+            batch = buffer.sample(BATCH_SIZE)
+            loss_v = model.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=GAMMA)
+            loss_v.backward()
+            losses.append(loss_v.item())
+            optimizer.step()
+
+            if step % NET_SYNC_STEPS == 0:
+                tgt_net.sync()
+            if len(losses) >= 100:
+                mean_loss = np.mean(losses)
+                log.info("%d: loss=%.3f", step, mean_loss)
+                losses.clear()
     pass
